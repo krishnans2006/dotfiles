59398d8e11c8b58e1e86fe3c31121e42060bf19d
Krishnan Shankar <krishnans2006@gmail.com>
2023-10-09 02:22:01 -0400

    Add Google Cloud Compute VM SSH alias

--- aliases.zsh
+++ aliases.zsh
@@ -89,6 +89,8 @@ alias hobby='cd ~/Tech/Hobby'
 alias piserver='ssh -J serveo.net krishnan@krishnans2006'  # SSH into my Raspberry Pi server, proxied through serveo.net
 alias piserver-local='ssh krishnan@piserver.local'
 
+alias gcloudvm='gcloud compute ssh krishnans2006@personal --project personal-vmserver --zone us-central1-f --ssh-key-file ~/.ssh/id_ed25519'
+
 # TJUAV
 
 alias tjuav='cd ~/Tech/TJUAV/'  # Shortcut to TJUAV directory
