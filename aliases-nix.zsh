713805fe1dd8dde887d80f75f2fa700b196a6c69
Krishnan Shankar <krishnans2006@gmail.com>
2025-01-29 17:14:58 -0600

    Fix typo in `g-size` alias

--- aliases-nix.zsh
+++ aliases-nix.zsh
@@ -17,7 +17,7 @@ alias dc='docker compose'  # Docker Compose
 alias pmpy='python manage.py'  # Django manage.py
 
 ## Git/GitHub
-alias g-size='git gc --quiet && git count-objects -vH | grep "size-pack "'
+alias g-size='git gc --quiet && git count-objects -vH | grep "size-pack: "'
 
 gc-past() {
     export GIT_AUTHOR_DATE="$1"
